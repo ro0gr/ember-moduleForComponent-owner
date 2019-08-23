c4310ab8db0d52eedbaeb7eaca632a23dca1cec4
Ruslan Hrabovyi <ruslan.hrabovyi@ligadigital.com>
2019-08-24 02:50:17 +0300

    add render test

---
+++ tests/integration/render-test.js
@@ -0,0 +1,10 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('calculating-device', 'Integration | context', {
+  integration: true
+});
+
+test('Test', async function(assert) {
+  this.render(hbs`test`);
+});
